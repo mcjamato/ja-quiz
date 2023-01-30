c575c12895730f9771a50737fba0a1a2c5eb1047
mcjamato <121120887+mcjamato@users.noreply.github.com>
2023-01-29 19:42:37 -0500

    Update game.js
    add new test questions 
    change test to have 10 questions 10 points each

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('questionsmidterm.json')
+fetch('questionsjoeSSc10L1.json')
     .then((res) => {
         return res.json();
     })
@@ -24,8 +24,8 @@ fetch('questionsmidterm.json')
     });
 
 //CONSTANTS
-const CORRECT_BONUS = 4;
-const MAX_QUESTIONS = 25;
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
