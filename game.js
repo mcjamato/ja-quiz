995b591432bfd28478ffe1ef4c45efec0d155a98
mcjamato <121120887+mcjamato@users.noreply.github.com>
2023-04-20 17:45:04 -0400

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('jsonfiles/questionsSSCH8L3.json')
+fetch('jsonfiles/questionsSSCH8.json')
     .then((res) => {
         return res.json();
     })
